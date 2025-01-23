b4e69dc0bd9095348a9fc01bece44b731e7dbb86
Fadil <mnurfadillah37@gmail.com>
2025-01-23 11:40:14 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register-user', function () {
+    return view('register');
+});
+
+Route::get('/login-user', function () {
+    return view('login');
+});
