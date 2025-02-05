943d2f0107b0d85e07fdf8199fc2b5ecfea4bc22
jodiferi <jodi.22092@mhs.unesa.ac.id>
2025-02-05 21:54:14 +0700

    test

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+                'name' => 'Test Pertama',
+                'description' => 'Role pertama dalam sistem',
+                'status' => 1, // 1 = aktif
+                'created_at' => now(),
+                'created_id' => 1, // Misal ID admin pertama
+                'update_at' => now(),
+                'update_id' => 1,
+            ],
+            [
+                'name' => 'Test Kedua',
+                'description' => 'Role kedua dalam sistem',
+                'status' => 1, // 1 = aktif
+                'created_at' => now(),
+                'created_id' => 1,
+                'update_at' => now(),
+                'update_id' => 1,
+            ]
+        ]);
+    }
+}
